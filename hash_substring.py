bd30e08a8de2255e8c5dcd7775ae2cbbef3ac348
Egils Kalns 191RBC023 <125309153+egilskalns@users.noreply.github.com>
2023-04-14 10:26:14 +0000

    1

--- hash_substring.py
+++ hash_substring.py
@@ -2,8 +2,19 @@
 
 def read_input():
     # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose
+    # which input type will follow
+    text = input().rstrip()
+    if text == "I":
+        pattern = input().rstrip()
+        data = input().rstrip()
+
+    elif text == "F":
+        file = open("tests/06","r")
+        text3 = file.read().rstrip().split("\n")
+        file.close()
+        pattern = text3[0].rstrip()
+        data = text3[1].rstrip()
     
     # after input type choice
     # read two lines 
@@ -13,7 +24,7 @@ def read_input():
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (pattern,data)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
@@ -21,10 +32,27 @@ def print_occurrences(output):
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
-
     # and return an iterable variable
-    return [0]
+    pattern_len = len(pattern)
+    text_len = len(text)
+    pattern_hash = get_hash(pattern)
+    occurances=[]
 
+    for i in range(text_len):
+        if text_len-i<pattern_len:
+            break
+        main_text_hash = get_hash(text[i:i+pattern_len])
+        if pattern_hash == main_text_hash:
+            if pattern == text[i:i+pattern_len]:
+                occurances.append(i)
+    return occurances
+
+def get_hash(pattern: str) -> int:
+    m = len(pattern)
+    result = 0
+    for i in range(m):
+        result = (13 * result + ord(pattern[i])) % 256
+    return result
 
 # this part launches the functions
 if __name__ == '__main__':
